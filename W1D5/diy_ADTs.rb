fb53d9aca2f2e1e9e041ade071ee26f802619d3a
DrewE <andrew.engelstein@gmail.com>
2019-01-10 21:14:14 -0800

    Add Map class with initial structure

--- W1D5/diy_ADTs.rb
+++ W1D5/diy_ADTs.rb
@@ -42,3 +42,23 @@ class Queue
     @queue.first
   end
 end
+
+# Map Class
+
+class Map
+  def initialize
+
+  end
+
+  def set(key, value)
+  end
+
+  def get(key)
+  end
+
+  def delete(key)
+  end
+
+  def show
+  end
+end
