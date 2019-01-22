062dbbafa316dede96c58f060ba7ad7e85995d69
DrewE <andrew.engelstein@gmail.com>
2019-01-21 22:43:35 -0800

    Add ::find_by_name(name), #create, #update to Playwright class

--- W3D2/skeleton/plays.rb
+++ W3D2/skeleton/plays.rb
@@ -81,18 +81,41 @@ class Playwright
   end
   
   def self.find_by_name(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+    SELECT *
+    FROM playwrights
+    WHERE name = ?;
+    SQL
+    data.map { |datum| Playwright.new(datum) }  
   end
-
+  
   def initialize(options)
     @id = options["id"]
     @name = options["name"]
     @birth_year = options["birth_year"]
   end
-
+  
   def create
+    raise "#{self} is already in the Database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO 
+        playwrights (name, birth_year)
+      VALUES 
+        (?, ?);
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
   end
-
+  
   def update
+    raise "#{self} not in Database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights (name, birth_year)
+      VALUES 
+        (?, ?)
+      WHERE
+        id = ?;
+    SQL
   end
 
   def get_plays
