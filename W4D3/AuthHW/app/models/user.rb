9a4788a0add2d1af625c47f0aace72cccf754a6c
DrewE <andrew.engelstein@gmail.com>
2019-01-31 11:14:54 -0800

    Add methods to User Model for validation and checking session tokens

--- W4D3/AuthHW/app/models/user.rb
+++ W4D3/AuthHW/app/models/user.rb
@@ -12,5 +12,41 @@
 
 class User < ApplicationRecord
   validates :username, :session_token, presence: true
-  validaters :password_digest, presence: {true, message: "Password can't be blank"}, length: {minimum: 6, allow_nil: true}
+  validates :username, uniqueness: true
+  validates :password_digest, presence: {message: "Password can't be blank"}, length: {minimum: 6, allow_nil: true}
+  validates :password, length: {minimum: 6, allow_nil: true}
+  before_validation :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password
+    @password
+  end
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.find_by_credentials(un, pw)
+    user = User.find_by(username: un)
+    return user if user.is_password?(pw)
+    nil
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
 end
