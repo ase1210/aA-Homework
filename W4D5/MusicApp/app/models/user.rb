dab91acc6e4f877c0bda68bd42e84e9d62c53ef8
DrewE <andrew.engelstein@gmail.com>
2019-02-01 08:58:32 -0800

    Copied Music App structure to HW folder for W4D5 HW

---
+++ W4D5/MusicApp/app/models/user.rb
@@ -0,0 +1,51 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  email           :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :email, :session_token, :password_digest, presence: true
+  validates :email, :session_token, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, pw)
+    user = User.find_by(email: email)
+    return nil unless user
+    user.is_password?(pw) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password
+    @password
+  end
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+end
