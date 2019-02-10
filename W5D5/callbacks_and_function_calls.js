0fd0dfbe5b9c7ecb20adfe9cc4e0512ad387bdc3
DrewE <andrew.engelstein@gmail.com>
2019-02-09 19:58:27 -0800

    Complete callbacks and functions hw problems

---
+++ W5D5/callbacks_and_function_calls.js
@@ -0,0 +1,9 @@
+// window.setTimeout(function () {
+//   alert("Hammertime!");
+// }, 5000);
+
+function hammerTime(time) {
+  setTimeout(() => {
+    alert(`${time} is hammertime!`);
+  }, 5000);
+}
---
+++ W5D5/cbs_and_fn_node.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function teaAndBiscuits() {
+  let first, second
+
+  reader.question("Would you like some tea? ", (answer) => {
+    first = answer;
+    console.log(`You replied ${answer}`)
+    reader.question("Would you like some biscuits? ", (response) => {
+      second = response;
+      console.log(`You replied ${response}`)
+      const firstRes = (first === 'yes') ? 'do' : 'don\'t';
+      const secondRes = (second === 'yes') ? 'do' : 'don\'t';
+      console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`)
+      reader.close();
+    });
+  });
+
+}
+
+// teaAndBiscuits();
