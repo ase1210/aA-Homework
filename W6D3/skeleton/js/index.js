260e82754b557b7d8a588d78f5bfec8a15063e2d
DrewE <andrew.engelstein@gmail.com>
2019-02-13 08:54:40 -0800

    Complete AJAX request hw

--- W6D3/skeleton/js/index.js
+++ W6D3/skeleton/js/index.js
@@ -1,5 +1,18 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
-
+$.ajax({
+  method: "GET",
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  dataType: 'json',
+  success: function (response) {
+    // debugger;
+    console.log(`The weather in ${response.name} is ${response.main.temp} kelvin with a humidity of ${response.main.humidity}`);
+  },
+  error: function (response) {
+    console.log("error");
+  }
+});
 // Add another console log here, outside your AJAX request
+
+console.log("After AJAX Request");
